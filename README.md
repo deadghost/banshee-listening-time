4cfb755ed91ceeba7598179b1de146d83d16cb7d
Kenny Liu <kennyliu@deadgho.st>
2014-03-09 23:56:56 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Keeps track of how much time is spent listening to music on banshee. Data is
+stored in a SQLite database.
---
+++ banshee-listening-time.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+create_table() {
+    sqlite3 time.db "CREATE TABLE IF NOT EXISTS time 
+    (id INTEGER PRIMARY KEY, date TEXT UNIQUE, seconds INTEGER);"
+}
+
+insert_replace_time() {
+    local seconds=$1
+    sqlite3 time.db \
+        "INSERT OR REPLACE INTO TIME (id, date, seconds) 
+         VALUES ((select id from time where date=date('now', 'localtime')),
+                date('now', 'localtime'), 
+                COALESCE(((SELECT seconds FROM time 
+                    WHERE date=date('now', 'localtime')) + $seconds), 
+                    $seconds));"
+}
+
+convert_seconds() {
+    (( h=${1}/3600 ))
+    (( m=${1}%3600/60 ))
+    (( s=${1}%60 ))
+    printf "%02d:%02d:%02d\n" $h $m $s
+}
+
+main() {
+    create_table
+    seconds=0
+    unix_time=`date +%s`
+
+    while sleep 10; do
+        if ( pgrep banshee > /dev/null ); then
+            if [ "$(banshee --query-current-state | cut -d ' ' -f2)" \
+                = "playing" ]; then
+                current_unix_time=`date +%s`
+                elapsed_seconds=$((current_unix_time - unix_time))
+                seconds=$((seconds + elapsed_seconds))
+                insert_replace_time $elapsed_seconds
+                #echo "Total listening time: $seconds seconds"
+                convert_seconds $seconds
+                unix_time=$current_unix_time
+            fi
+        fi
+    done
+}
+
+main
